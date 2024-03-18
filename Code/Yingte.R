5b801892e2f199b043b7c43195ce8c94e84256f5
Yingte Liu <yli2932@emory.edu>
2024-03-18 19:09:31 -0400

    save the plotsR

--- Code/Yingte.R
+++ Code/Yingte.R
@@ -5,9 +5,15 @@ path = here()
 df = read.csv(paste(path,"/Raw_Data/f75_interim.csv",sep=""))
 df$treatment = ifelse(df$arm=='Standard F75',"standard","intervention")
 
-mosaic( ~  treatment + hiv_results + sex, highlighting = "treatment",highlighting_fill = c("lightblue", "pink"),data = df)
+mosaic_plot = mosaic( ~  treatment + hiv_results + sex, highlighting = "treatment",highlighting_fill = c("lightblue", "pink"),data = df)
 
 #interpretation: We stratified the patients based on their gender, hiv-testing results, and treatments they received. 
 # Most male and female babies participated in this studies have negative result for hiv-testing
 # Standard F75 and intervention are given to both female and male babies in a 50-50 ratio 
-#a 
+#added some context
+
+
+saveRDS(
+  mosaic_plot,
+  file = here::here("output", "mosaic_plot.rds")
+)
