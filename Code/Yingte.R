54972d37b11bd31a5452b066e57872c72ba5b6db
Tracy Huang <tracy.huang@emory.edu>
2024-03-18 22:08:44 -0400

    commit changes

--- Code/Yingte.R
+++ Code/Yingte.R
@@ -1,18 +1,24 @@
+# load packages
 library(vcd)
 library(here)
+
+# define path
 here::i_am("Code/Yingte.R")
 path = here()
+
+# read in data and clean
 df = read.csv(paste(path,"/Raw_Data/f75_interim.csv",sep=""))
 df$treatment = ifelse(df$arm=='Standard F75',"standard","intervention")
 
-mosaic_plot = mosaic( ~  treatment + hiv_results + sex, highlighting = "treatment",highlighting_fill = c("lightblue", "pink"),data = df)
+# create mosaic plot
+mosaic_plot = mosaic( ~  treatment + hiv_results + sex, highlighting = "treatment", highlighting_fill = c("lightblue", "pink"),data = df)
 
 #interpretation: We stratified the patients based on their gender, hiv-testing results, and treatments they received. 
 # Most male and female babies participated in this studies have negative result for hiv-testing
 # Standard F75 and intervention are given to both female and male babies in a 50-50 ratio 
 #added some context
 
-
+# save mosaic plot
 saveRDS(
   mosaic_plot,
   file = here::here("output", "mosaic_plot.rds")
